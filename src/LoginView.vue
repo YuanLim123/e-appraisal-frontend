0225326113bf9257a63570f8347f79bea44d6376
YuanLim123 <yuanlim2000@hotmail.com>
2025-10-19 14:47:32 +0800

    feat: add validation and icon spinner component

--- src/LoginView.vue
+++ src/LoginView.vue
@@ -36,6 +36,7 @@ onBeforeUnmount(store.resetForm);
               class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
             />
           </div>
+          <ValidationError :errors="store.errors" field="username" />
         </div>
 
         <div>
@@ -59,13 +60,15 @@ onBeforeUnmount(store.resetForm);
               class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
             />
           </div>
+          <ValidationError :errors="store.errors" field="password" />
         </div>
 
         <div>
           <button
             type="submit"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            class="flex w-full justify-center gap-2 rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
+            <IconSpinner v-show="store.loading" />
             Sign in
           </button>
         </div>
